40739644a23116d581a0d4a7bb4cfe6d1ee32c35
Aditya5191 <aditya.jemshetty@gmail.com>
2025-06-06 01:18:58 +0530

    some new files

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from send_images import ZMQCameraPublisher
+from recieve import ZMQArrowReceiver
+import threading
+
+angle=0
+distance=0
+direction=""
 
 camera= ZMQCameraPublisher()
-camera.publish_frames() 
+pc_response =  ZMQArrowReceiver()
+ 
+
+def get_response():
+    while True:
+        angle, distance, direction = pc_response.receive_arrow_data()
+        print(angle,distance,direction)
+
+camera_thread = threading.Thread(target=camera.publish_frames)
+response_thread = threading.Thread(target=get_response)
+
+camera_thread.start()
+response_thread.start()
