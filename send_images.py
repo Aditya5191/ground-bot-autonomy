153dda1e6de4911f18068d5437a2e3b6f17c9d00
Aditya5191 <aditya.jemshetty@gmail.com>
2025-06-06 00:33:47 +0530

    pi is publisher

--- send_images.py
+++ send_images.py
@@ -1,28 +1,22 @@
 import zmq
 import cv2
-import base64
 import time
 from picamera2 import Picamera2
 
-class ZMQCameraClient:
+class ZMQCameraPublisher:
     def __init__(self):
         self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PUSH)
-        self.socket.connect("tcp://192.168.1.100:5555")  # Replace with server IP and port
+        self.socket = self.context.socket(zmq.PUB)
+        self.socket.bind("tcp://*:5555")  # Bind to all interfaces on port 5555
 
         self.picam2 = Picamera2()
         self.picam2.start()
 
-    def camera_sender(self):
+    def publish_frames(self):
         while True:
             frame = self.picam2.capture_array()
             frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            _, buffer = cv2.imencode('.jpg', frame_bgr, [int(cv2.IMWRITE_JPEG_QUALITY), 70])  # optional: lower quality
-            self.socket.send(buffer.tobytes())
+            _, buffer = cv2.imencode('.jpg', frame_bgr, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
+            self.socket.send(buffer.tobytes())  # Send raw JPEG bytes
             
             time.sleep(0.1)  # 10 FPS
-
-
-# To run
-# client = ZMQCameraClient()
-# client.camera_sender()
